52936c548e83e54e45be9bb5f5c286f4d23060b1
5kvariety <126929458+5kvariety@users.noreply.github.com>
2023-03-28 02:45:34 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    token: "MTAxNzkyNDE0OTkyODI4MDA5NA.GHrYut.7LJMM-b6z64yx7bYTlEXvY1vCuFgDa_hEW4YbA", // place your token here (no token = QR Login)
-    password: "Lolirock0+", // The password is mandatory to change the nickname & tag
-    username: "! Sans", // The username to snipe (no username === no work)
-    tag: "0527" // The tag to snipe (no tag === no work)
+    token: "MTA2NDE4NzgxNjM3MzkyNzkzNg.G77QcR.rwYzKkFul5168_BRBvWGWHvRiB04g-zPxdquM8"", // place your token here (no token = QR Login)
+    password: "salam772", // The password is mandatory to change the nickname & tag
+    username: "blitz", // The username to snipe (no username === no work)
+    tag: "2021" // The tag to snipe (no tag === no work)
 }
